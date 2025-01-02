6ccf98e93773356f8f39df5169db3dfec64efc0c
Vinay Patil <itsvinaypatil9663@gmail.com>
2025-01-02 22:49:30 +0530

    Secomd commit

---
+++ src/test/java/testCases/TC_004_SimpleTestforGit.java
@@ -0,0 +1,18 @@
+package testCases;
+
+import org.testng.annotations.Test;
+
+import testBase.BaseClass;
+
+public class TC_004_SimpleTestforGit extends BaseClass {
+
+	
+	@Test
+	public void test()
+	{
+		logger.info("The simple test started");
+		
+		System.out.println("Simple Test for Github");
+	}
+	
+}
